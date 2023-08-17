94d11dd88836436e7198d930e0412322b9ad676f
mkarabo <mkarabo7@gmail.com>
2023-08-17 01:17:17 -0700

    more structures updated

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-num = 0
-list_keys = list(a_dictionary.keys())
-
-for i in list_keys:
-num += 1
-
-return (num)
-
+    num = len(a_dictionary)
+    return (num)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-new_dir = a_dictionary.copy()
-list_keys = list(new_dir.keys())
-
-for i in list_keys:
-new_dir[i] *= 2
-
-return (new_dir)
+    new_dir = a_dictionary.copy()
+    for key in new_dir:
+        new_dir[key] *= 2
+    return (new_dir)
