fd96e8798da9753b0b7a729b7a4ec5285cfddc37
mkarabo <mkarabo7@gmail.com>
2023-08-17 00:08:42 -0700

    more structures updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-new_matrix = matrix.copy()
+def square_matrix_simple(matrix=None):
+    if matrix is None:
+        matrix = []
 
-for i in range(len(matrix)):
-	new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    new_matrix = matrix.copy() 
 
-return (new_matrix)
+    for i in range(len(matrix)):  
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))  
+
+    return (new_matrix)
